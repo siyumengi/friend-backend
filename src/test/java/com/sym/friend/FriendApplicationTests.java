5ee4535c7d0d02710eb8f5ff3a0a9f364f01636c
siyumeng <1135306192@qq.com>
2023-05-28 22:45:35 +0800

    初始化了后台项目。

---
+++ src/test/java/com/sym/friend/FriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sym.friend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
